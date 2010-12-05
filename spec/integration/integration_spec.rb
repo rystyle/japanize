775180282a6754240cff50445ade2f2d3834c146
roco <roco@example.com>
2010-12-05 09:55:20 +0900

    looking at .

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -11,17 +11,21 @@ describe "Japanization" do
       "１　に　２　を　たす".japanize.must_equal 3
     end
   end
-  
+
   it "must calculate single math operation" do
      (１　に　２　を　たす).must_equal 3
   end
-  
+
+  it "must calculate with . in math operation" do
+     (１　に　２.２　を　たす).must_equal 3.2
+  end
+
   it "must calculate compound math operation" do
     (１　に　２　を　たし　て　３　を　かける).must_equal 9
   end
-  
+
   it "must calculate compound math operation　phrase" do
     (１に２をたして３をかける).must_equal 9
   end
-  
+
 end
